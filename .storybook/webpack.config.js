81030fea7704ae2e323a317ab164a4a4abb96e7f
Ivan Demchenko <i.demchenko@instamotion.com>
2019-10-14 15:05:07 +0200

    Added header and branding logos

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,13 @@
-const { join } = require('path');
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = ({ config }) => {
+
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: 'awesome-typescript-loader'
   });
   config.resolve.extensions.push('.ts', '.tsx');
+
   return config;
 };
