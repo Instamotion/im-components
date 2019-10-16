922f9fa6b63b456cf4123a891ce9ca050f027a78
Ivan Demchenko <i.demchenko@instamotion.com>
2019-10-16 14:20:03 +0200

    cleaned up tests

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,5 @@ const base = require('./configs/jest.config');
 
 module.exports = {
     ...base,
-    projects: [
-      "<rootDir>/packages/*/jest.config.js"
-    ],
     coverageDirectory: "<rootDir>/coverage/"
 };
--- packages/header/jest.config.js
+++
@@ -1,8 +0,0 @@
-const base = require("../../jest.config");
-const pack = require("./package");
-
-module.exports = {
-  ...base,
-  displayName: pack.name,
-  name: pack.name
-};
