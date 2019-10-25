93268c2393f700d4e7dc69331bee37134be12cd4
Maksym Kolesnikov <maxim.it.ua@gmail.com>
2019-10-25 12:05:30 +0300

    missed localization

--- packages/header/utils/locales.ts
+++ packages/header/utils/locales.ts
@@ -3,7 +3,7 @@ const de = {
   'header.menu.search': 'Suchen',
   'header.menu.top_offers': 'Topangebote',
   'header.menu.wish_list': 'Merkzettel',
-  'header.menu.phone_number': '089 411151-100',
-}
+  'header.menu.phone_number': '089 411151-100'
+};
 
 export default { de };
