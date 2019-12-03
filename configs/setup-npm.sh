a0f1b21c213105260fea01cfdb573edcdf97fbb2
Ivan Demchenko <i.demchenko@instamotion.com>
2019-12-03 10:44:45 +0100

    fix(jenkins): i still have no idea what im doing

--- configs/setup-npm.sh
+++ configs/setup-npm.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-if [ "$GIT_SSH_KEY" != "" ]; then
-  echo "Detected SSH key for git. Adding SSH config" >&1
-  echo "" >&1
+# if [ "$GIT_SSH_KEY" != "" ]; then
+#   echo "Detected SSH key for git. Adding SSH config" >&1
+#   echo "" >&1
 
-  if [ ! -d ~/.ssh ]; then
-    mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
-  fi
+#   if [ ! -d ~/.ssh ]; then
+#     mkdir -p ~/.ssh
+#     chmod 700 ~/.ssh
+#   fi
 
-  echo $GIT_SSH_KEY | base64 -d > ~/.ssh/deploy_key
-  chmod 600 ~/.ssh/deploy_key
-  echo -e "Host github.com\n"\
-          " IdentityFile ~/.ssh/deploy_key\n"\
-          " IdentitiesOnly yes\n"\
-          " UserKnownHostsFile=/dev/null\n"\
-          " StrictHostKeyChecking no"\
-          > ~/.ssh/config
-fi
+#   echo $GIT_SSH_KEY | base64 -d > ~/.ssh/deploy_key
+#   chmod 600 ~/.ssh/deploy_key
+#   echo -e "Host github.com\n"\
+#           " IdentityFile ~/.ssh/deploy_key\n"\
+#           " IdentitiesOnly yes\n"\
+#           " UserKnownHostsFile=/dev/null\n"\
+#           " StrictHostKeyChecking no"\
+#           > ~/.ssh/config
+# fi
 
-# echo -e "\e[32m  Setting git configs..."
-# git config --global user.email "it-systems@instamotion.com"
-# git config --global user.name "im-it-systems"
-# git config --global push.default simple
+echo -e "\e[32m  Setting git configs..."
+git config --global user.email "i.demchenko@instamotion.com"
+git config --global user.name "raqystyle"
+git config --global push.default simple
 
-# echo -e "\e[32m  Setting git remote..."
-# git remote add github https://$GH_USER:$GH_PASS@github.com/Instamotion/im-components.git
+echo -e "\e[32m  Setting git remote..."
+git remote add github https://raqystyle:$GH_TOKEN@github.com/Instamotion/im-components.git
 
 echo -e "\e[32m  Setting npm tokens"
 npm set //registry.npmjs.org/:_authToken=$NPM_PUB_TOKEN
