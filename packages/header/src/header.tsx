d931ebc68c654e88b82de697217666026cb02a47
Giovanni Esposito <g.esposito@instamotion.com>
2019-12-20 08:54:19 +0100

    Fix header (#32)
    * fix: fix z-index to high
    
    * docs: changelog

--- packages/header/src/header.tsx
+++ packages/header/src/header.tsx
@@ -118,7 +118,7 @@ export const HeaderWrapper = styled.header<HeaderWrapperProps>`
   transition: all 0.3s ease;
   display: flex;
   position: fixed;
-  z-index: 1000000;
+  z-index: 10;
   top: 0;
   right: 0;
   left: 0;
