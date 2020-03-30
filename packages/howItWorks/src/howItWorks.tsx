5dcc7a317fac6b4a4ec198b94822bfbc1ab5fcb2
Dmytro Bezvoshchuk <d.bezvoshchuk@instamotion.com>
2020-03-30 22:57:36 +0200

    RT-333: Update HowItWorks component

--- packages/howItWorks/src/howItWorks.tsx
+++ packages/howItWorks/src/howItWorks.tsx
@@ -16,79 +16,71 @@ const Header = styled(Heading)`
   margin: 0 0 1rem;
   font-weight: bold;
 
-  ${theme.mediaQueries.whenDesktop} {
+  ${theme.mediaQueries.whenTablet} {
     margin-bottom: 1.5rem;
     font-size: 1.5rem;
   }
 `;
 
 const List = styled.ol`
+  display: flex;
+  flex-direction: column;
   padding: 0;
   list-style: none;
   counter-reset: item;
 
-  ${theme.mediaQueries.whenDesktop} {
-    display: flex;
-    justify-content: space-between;
+  ${theme.mediaQueries.whenTablet} {
+    flex-direction: row;
     margin-bottom: 1rem;
   }
 `;
 
 const ListItem = styled.li`
+  display: flex;
+  flex-direction: row;
   position: relative;
-  counter-increment: item;
   color: ${theme.color.brightGrey};
-  line-height: 1rem;
 
-  ${theme.mediaQueries.whenDesktop} {
+  ${theme.mediaQueries.whenTablet} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 25%;
     font-size: ${px2rem(18)};
-  }
-
-  &:before {
-    display: inline-block;
-    width: 1rem;
-    height: 1rem;
-    margin-right: 0.75rem;
-    font-size: 0.75rem;
-    content: counter(item);
     text-align: center;
-    vertical-align: middle;
-    color: ${theme.color.white};
-    background: ${theme.color.downy};
-    border-radius: 100%;
 
-    ${theme.mediaQueries.whenDesktop} {
-      display: block;
-      margin: 0 auto 1rem;
-      width: 1.5rem;
-      height: 1.5rem;
-      font-size: 1rem;
-      line-height: 1.5rem;
-      border: 0.5rem solid ${theme.color.lightGrey};
+    &:first-child {
+      align-items: flex-start;
+    }
+
+    &:last-child {
+      align-items: flex-end;
     }
   }
 `;
 
-const DesktopDivider = styled.hr`
-  display: none;
+const Number = styled.span`
+  width: 1rem;
+  height: 1rem;
+  margin: 0 0.75rem 0 0;
+  line-height: 1rem;
+  font-size: 0.75rem;
+  text-align: center;
+  color: ${theme.color.white};
+  background: ${theme.color.downy};
+  border-radius: 100%;
 
-  ${theme.mediaQueries.whenDesktop} {
-    display: block;
-    color: ${theme.color.downy};
-    width: 89%;
-    margin-bottom: -2.25rem;
+  ${theme.mediaQueries.whenTablet} {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin: 0 0 1rem;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    border: 0.5rem solid ${theme.color.lightGrey};
   }
 `;
 
-const MobileDivider = styled.span`
-  display: inline-block;
-  margin: 0.3rem 0 0 0.4rem;
-  color: ${theme.color.downy};
-
-  ${theme.mediaQueries.whenDesktop} {
-    display: none;
-  }
-`;
+const Text = styled.span``;
 
 const TooltipText = styled.div`
   display: none;
@@ -99,11 +91,10 @@ const TooltipText = styled.div`
   border-radius: 0.25rem;
   box-shadow: 0 0.5rem 0.5rem 0 rgba(0, 0, 0, 0.1);
 
-  ${theme.mediaQueries.whenDesktop} {
+  ${theme.mediaQueries.whenTablet} {
     position: absolute;
-    width: 150%;
-    left: -25%;
     top: 5.5rem;
+    left: 0;
   }
 `;
 
@@ -115,12 +106,34 @@ const Tooltip = styled.span`
   display: inline-block;
   margin-left: 0.5rem;
 
-  &:hover {
+  &:hover,
+  &:active {
     cursor: pointer;
 
     ${TooltipText} {
       display: inline;
     }
+  }
+`;
+
+const DesktopDivider = styled.hr`
+  display: none;
+
+  ${theme.mediaQueries.whenTablet} {
+    display: block;
+    color: ${theme.color.downy};
+    width: 99%;
+    margin-bottom: -2.3rem;
+  }
+`;
+
+const MobileDivider = styled.span`
+  display: inline-block;
+  margin: 0.3rem 0.4rem 0.2rem;
+  color: ${theme.color.downy};
+
+  ${theme.mediaQueries.whenTablet} {
+    display: none;
   }
 `;
 
@@ -139,49 +152,61 @@ const HowItWorksComponent: React.FC<HowItWorksProps> = ({ className }) => (
 
     <List>
       <ListItem>
-        <FormattedMessage id="how_it_works.step_1" />
-        <Tooltip>
-          <TooltipIcon icon={'infoCircle'} />
-          <TooltipText>
-            <FormattedMessage id="how_it_works.tooltip_1" />
-          </TooltipText>
-        </Tooltip>
+        <Number>1</Number>
+        <Text>
+          <FormattedMessage id="how_it_works.step_1" />
+          <Tooltip>
+            <TooltipIcon icon={'infoCircle'} />
+            <TooltipText>
+              <FormattedMessage id="how_it_works.tooltip_1" />
+            </TooltipText>
+          </Tooltip>
+        </Text>
       </ListItem>
 
       <MobileDivider>|</MobileDivider>
 
       <ListItem>
-        <FormattedMessage id="how_it_works.step_2" />
-        <Tooltip>
-          <TooltipIcon icon={'infoCircle'} />
-          <TooltipText>
-            <FormattedMessage id="how_it_works.tooltip_2" />
-          </TooltipText>
-        </Tooltip>
+        <Number>2</Number>
+        <Text>
+          <FormattedMessage id="how_it_works.step_2" />
+          <Tooltip>
+            <TooltipIcon icon={'infoCircle'} />
+            <TooltipText>
+              <FormattedMessage id="how_it_works.tooltip_2" />
+            </TooltipText>
+          </Tooltip>
+        </Text>
       </ListItem>
 
       <MobileDivider>|</MobileDivider>
 
       <ListItem>
-        <FormattedMessage id="how_it_works.step_3" />
-        <Tooltip>
-          <TooltipIcon icon={'infoCircle'} />
-          <TooltipText>
-            <FormattedMessage id="how_it_works.tooltip_3" />
-          </TooltipText>
-        </Tooltip>
+        <Number>3</Number>
+        <Text>
+          <FormattedMessage id="how_it_works.step_3" />
+          <Tooltip>
+            <TooltipIcon icon={'infoCircle'} />
+            <TooltipText>
+              <FormattedMessage id="how_it_works.tooltip_3" />
+            </TooltipText>
+          </Tooltip>
+        </Text>
       </ListItem>
 
       <MobileDivider>|</MobileDivider>
 
       <ListItem>
-        <FormattedMessage id="how_it_works.step_4" />
-        <Tooltip>
-          <TooltipIcon icon={'infoCircle'} />
-          <TooltipText>
-            <FormattedMessage id="how_it_works.tooltip_4" />
-          </TooltipText>
-        </Tooltip>
+        <Number>4</Number>
+        <Text>
+          <FormattedMessage id="how_it_works.step_4" />
+          <Tooltip>
+            <TooltipIcon icon={'infoCircle'} />
+            <TooltipText>
+              <FormattedMessage id="how_it_works.tooltip_4" />
+            </TooltipText>
+          </Tooltip>
+        </Text>
       </ListItem>
     </List>
 
@@ -200,12 +225,9 @@ const HowItWorks = styled(HowItWorksComponent)`
   border-top: 1px solid ${theme.color.silver};
   border-bottom: 1px solid ${theme.color.silver};
 
-  ${theme.mediaQueries.whenDesktop} {
+  ${theme.mediaQueries.whenTablet} {
     padding: 1.5rem 3rem;
   }
-
-  /* todo: delete fonts ijection */
-  font-family: Roboto, sans-serif;
 `;
 
 export default HowItWorks;
