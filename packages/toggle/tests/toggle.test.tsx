f9fbe4c31764409c29e36090e65b418a6b6eedfd
Slava Lebedev <slle@opinov8.com>
2020-04-06 10:39:32 +0300

    RT-442: update toggle test;

--- packages/toggle/tests/toggle.test.tsx
+++ packages/toggle/tests/toggle.test.tsx
@@ -1,38 +1,38 @@
 /* eslint-env jest */
 import React from 'react';
 import { mount } from 'enzyme';
-import { renderWithThemeAdnI18n } from '@lib/utils';
-import Toggle, { ToggleProps, WrapperLabel, ToggleInput } from '../../src/Toggle';
+import { renderWithThemeAndI18n } from '@im-ui/utils';
+import Toggle, { ToggleProps, WrapperLabel, ToggleInput } from '../src/toggle';
 
 const onChangeEvent = jest.fn();
 
 describe('Toggle', () => {
-  const props: ToggleProps = {
+  const props = {
     id: 'switcher id',
     onChange: onChangeEvent,
     messageId: 'Action name'
   };
   it('renders switcher component', () => {
-    const wrapper = mount(renderWithThemeAdnI18n(<Toggle {...props} />));
+    const wrapper = mount(renderWithThemeAndI18n(<Toggle {...props} />));
     expect(wrapper.find(Toggle)).toHaveLength(1);
     expect(wrapper.find(WrapperLabel)).toEqual({});
   });
 
   it('renders switcher with label', () => {
     const wrapper = mount(
-      renderWithThemeAdnI18n(<Toggle {...props} messageId="serp.filters.topAngebote" />)
+      renderWithThemeAndI18n(<Toggle {...props} messageId="serp.filters.topAngebote" />)
     );
     expect(wrapper.find(Toggle)).toHaveLength(1);
     expect(wrapper.find(WrapperLabel)).toHaveLength(1);
   });
 
   it('renders switcher disabled', () => {
-    const wrapper = mount(renderWithThemeAdnI18n(<Toggle {...props} disabled />));
+    const wrapper = mount(renderWithThemeAndI18n(<Toggle {...props} disabled />));
     expect(wrapper.find(ToggleInput).prop('disabled')).toEqual(true);
   });
 
   it('switcher event is triggered', () => {
-    const wrapper = mount(renderWithThemeAdnI18n(<Toggle {...props} onChange={onChangeEvent} />));
+    const wrapper = mount(renderWithThemeAndI18n(<Toggle {...props} onChange={onChangeEvent} />));
     expect(wrapper).toBeDefined();
 
     wrapper
