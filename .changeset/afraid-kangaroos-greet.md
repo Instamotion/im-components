4864d151a65cf6c539c8ddf807378bcf9bc649c1
Ivan Demchenko <raqy.style@gmail.com>
2020-05-11 16:44:28 +0200

    cleaning up toggle components

---
+++ .changeset/afraid-kangaroos-greet.md
@@ -0,0 +1,5 @@
+---
+'@im-ui/toggle': patch
+---
+
+cleaning up toggle component
--- packages/toggle/utils/locales.ts
+++
@@ -1,6 +0,0 @@
-const de = {
-  'car.tile.addedToFavorite': 'Zur Merkliste hinzugefügt',
-  'car.tile.removedFromFavorite': 'Von Merkliste entfernt'
-};
-
-export default { de };
