0760dbf302d5f833efb3dd3823b8293b8805cc04
Max Alekseev <alekseev.maxim.nik@gmail.com>
2020-08-06 12:28:40 +0300

    RT-627: added gtm init event for A/B tests

--- packages/footer/src/footer.tsx
+++ packages/footer/src/footer.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import CheckoutFooter from './checkoutFooter';
 import DefaultFooter, { MenuOptions } from './defaultFooter';
 import TrustfulSection from './trustfulSection';
+import TagManager from 'react-gtm-module';
+
+const AB_TEST_VARIABLE_NAME: string = 'ab-test-product';
+
+type ProductAbVariationType = boolean | string;
 
 type FullFooterProps = {
   className?: string;
@@ -10,16 +15,41 @@ type FullFooterProps = {
   facebookToken: string;
   menuOptions?: MenuOptions;
   showEnvkv?: boolean;
+  gtmId: string;
+  productAbToggleVariable: string;
+  originalPhotosVariation: ProductAbVariationType;
 };
 
 type MinimalFooterProps = {
   className?: string;
   variant: 'minimal';
+  gtmId: string;
+  productAbToggleVariable: string;
+  originalPhotosVariation: ProductAbVariationType;
 };
 
 export type FooterProps = FullFooterProps | MinimalFooterProps;
 
 const Footer: React.FC<FooterProps> = props => {
+  if (props.productAbToggleVariable) {
+    const productTestVariation: ProductAbVariationType = useMemo(
+      () => props.originalPhotosVariation || 'default',
+      [props.originalPhotosVariation]
+    );
+
+    const dataLayerArgs = useMemo(
+      () => ({
+        gtmId: props.gtmId || 'GTM-5TPWWH',
+        dataLayer: {
+          [AB_TEST_VARIABLE_NAME]: `${props.productAbToggleVariable}-${productTestVariation}`
+        }
+      }),
+      [props.gtmId, AB_TEST_VARIABLE_NAME, props.productAbToggleVariable, productTestVariation]
+    );
+
+    TagManager.initialize(dataLayerArgs);
+  }
+
   switch (props.variant) {
     case 'minimal': {
       return <CheckoutFooter className={props.className} />;
