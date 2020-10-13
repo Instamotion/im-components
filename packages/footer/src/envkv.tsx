aac2a49cfa9a774cbf630f767196ccdccd2ab40c
Maxim Alekseev <alekseev.maxim.nik@gmail.com>
2020-10-13 15:16:23 +0300

    Fix/ut 138 padding (#113)
    * RT-138: fixed envkv padding
    
    * RT-138: changeset

--- packages/footer/src/envkv.tsx
+++ packages/footer/src/envkv.tsx
@@ -14,19 +14,19 @@ const ENVKVText = styled(Text)`
   line-height: 1.33;
   margin: 1rem auto;
   max-width: 80rem;
-  padding: 0 2rem 0 1rem;
+  padding: 0 2rem 0 2rem;
   color: ${theme.color.brightGrey};
 
   &::before {
     content: '*';
     position: absolute;
     font-size: 1.25rem;
-    left: 0;
+    left: 1rem;
     top: -0.25rem;
   }
 
   ${theme.mediaQueries.whenMobileL} {
-    padding-left: 1.5rem;
+    padding-left: 2.5rem;
     box-sizing: border-box;
   }
 `;
