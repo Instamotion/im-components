b4495d0e28215439423f8d04011c2fd1b0de94e4
AntonTretiak <tretiak.anton.dev@gmail.com>
2020-12-23 14:03:35 +0200

    add additional checks

--- packages/footer/utils/cookieProBanner.ts
+++ packages/footer/utils/cookieProBanner.ts
@@ -2,6 +2,6 @@ export const openBanner = (e: React.MouseEvent<EventTarget>) => {
   e.preventDefault();
   const cookieOpenBttn = document.getElementsByClassName(
     'ot-floating-button__open'
-  )[0] as HTMLButtonElement;
-  cookieOpenBttn.click();
+  )?.[0] as HTMLButtonElement;
+  cookieOpenBttn?.click();
 };
