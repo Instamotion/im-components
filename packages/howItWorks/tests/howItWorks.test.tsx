87292cef2abc31fe01ce5611b4b95cafdd99aefb
Max Alekseev <alekseev.maxim.nik@gmail.com>
2021-01-19 18:48:02 +0200

    UT-445: legacy patch

--- packages/howItWorks/tests/howItWorks.test.tsx
+++ packages/howItWorks/tests/howItWorks.test.tsx
@@ -2,22 +2,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { renderWithThemeAndI18n } from '@im-ui/utils';
-import HowItWorks, { HowItWorksProps } from '../src';
+import HowItWorks from '../src';
 import messages from '../utils/locales';
 
 describe('howItWorks', () => {
   it('How It Works component renders', () => {
-    const props: HowItWorksProps = {
-      currentStep: 1,
-      items: [
-        'Selecting the car and configure financing',
-        'Request the car and financing',
-        'Financing confirmation',
-        'Rechnungserhalt'
-      ]
-    };
-
-    const wrapper = mount(renderWithThemeAndI18n(<HowItWorks {...props} />, 'de', messages));
+    const wrapper = mount(renderWithThemeAndI18n(<HowItWorks />, 'de', messages));
 
     expect(wrapper).toBeDefined();
   });
