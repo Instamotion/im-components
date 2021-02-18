550c88303c48414732136dc2d648bcdfe968dd11
AntonTretiak <tretiak.anton.dev@gmail.com>
2021-02-18 20:57:34 +0200

    new theme

---
+++ packages/theme/src/IMTheme/IMInterface.ts
@@ -0,0 +1,142 @@
+type HexColor = string;
+
+type Heading = {
+  h1Mobile: number;
+  h1: number;
+  h2Mobile: number;
+  h2: number;
+  h3Mobile: number;
+  h3: number;
+  h4Mobile: number;
+  h4: number;
+};
+
+interface Border {
+  color: HexColor;
+  width: number;
+  radius?: number;
+}
+
+interface Line {
+  height: number;
+}
+
+interface Letter {
+  spacing: number;
+}
+
+export interface Font {
+  family: string;
+  size?: number;
+  height?: number;
+  spacing?: number;
+  weight?: number;
+}
+
+interface TableSection {
+  font: Font;
+  background: HexColor;
+}
+
+interface Button {
+  border: Border;
+  font: Font;
+  color: HexColor;
+  background: HexColor;
+  padding: string;
+  hover: {
+    shadow: string;
+  };
+  active: {
+    shadow: string;
+  };
+  disabled: {
+    opacity: number;
+  };
+}
+
+export type ColorLibrary = {
+  primary: HexColor;
+  secondary: HexColor;
+  niagara: HexColor;
+  downy: HexColor;
+  parisDaisy: HexColor;
+  white: HexColor;
+  flamePea: HexColor;
+  oil: HexColor;
+  brightGrey: HexColor;
+  silver: HexColor;
+  lightGrey: HexColor;
+  lighterGrey: HexColor;
+  grey: HexColor;
+  dark: HexColor;
+  black: HexColor;
+  cyan: HexColor;
+  silverDark: HexColor;
+  darkMailContent: HexColor;
+  almond: HexColor;
+  lightGreyBG: HexColor;
+  lightBackground: HexColor;
+  secondaryBlack: HexColor;
+  typo: HexColor;
+};
+
+export type FontSizes = {
+  lg: string;
+  normal: string;
+  sm: string;
+};
+
+export default interface IMTheme {
+  name: string;
+  color: ColorLibrary;
+  fontSize: FontSizes;
+  mediaQueries: {
+    whenMobile: string;
+    whenMobileL: string;
+    whenTablet: string;
+    whenDesktop: string;
+    whenDesktopL: string;
+    whenDesktopXL: string;
+    whenDesktopXXL: string;
+  };
+  table: {
+    outerBorder: Border;
+    innerBorder: Border;
+    head: TableSection;
+    body: TableSection;
+    foot: TableSection;
+  };
+  card: {
+    border: {
+      radius: number;
+    };
+    shadow: string;
+  };
+  heading: Heading;
+  button: {
+    primary: Button;
+    secondary: Button;
+    grayscale: Button;
+    outlined: Button;
+  };
+  input: {
+    border: Border;
+    font: Font;
+  };
+  label: {
+    font: Font;
+    line: Line;
+    letter: Letter;
+  };
+  font: {
+    sans: Font;
+    bentonRegular: Font;
+    bentonMedium: Font;
+    bentonBold: Font;
+    bentonBlack: Font;
+  };
+  footer: {
+    maxWidth: number;
+  };
+}
---
+++ packages/theme/src/IMTheme/IMTheme.ts
@@ -0,0 +1,281 @@
+import IMTheme, { Font } from './IMInterface';
+
+const sansSerifFont: Font = {
+  family: 'Roboto, sans-serif'
+};
+const BentonRegular: Font = {
+  family: 'BentonSans Regular',
+  weight: 400
+};
+const BentonMedium: Font = {
+  family: 'BentonSans Medium',
+  weight: 500
+};
+const BentonBold: Font = {
+  family: 'BentonSans Bold',
+  weight: 700
+};
+const BentonBlack: Font = {
+  family: 'BentonSans Black',
+  weight: 900
+};
+
+const colors = {
+  // primary
+  primary: '#054256',
+  // secondary
+  secondary: '#F7B628',
+  // dark green
+  niagara: '#20AD90',
+  // branding green
+  downy: '#75DFC0',
+  // warning
+  parisDaisy: '#FFE44C',
+  // default background
+  white: '#FFFFFF',
+  // required, error
+  flamePea: '#B75C50',
+  // font default
+  oil: '#444444',
+  //
+  brightGrey: '#606164',
+  // disabled
+  silver: '#C4C4C4',
+  // light-grey
+  lightGrey: '#FAFAFA',
+  // lighter-gray
+  lighterGrey: '#f3f3f3',
+  // grey
+  grey: '#9E9E9E',
+  // dark grey
+  dark: '#323330',
+  // black
+  black: '#000000',
+  // light shade of cyan
+  cyan: '#6DC4CD',
+  // silverDark
+  silverDark: '#666666',
+  // darkMailContent
+  darkMailContent: '#323330',
+  // faux-blanched almond:
+  almond: '#FBEEC7',
+  // USP background light gray color
+  lightGreyBG: '#F9F9F9',
+  //lightBackground
+  lightBackground: '#F9F9F9',
+  //secondaryBlack
+  secondaryBlack: '#333333',
+  //typo
+  typo: '#575756'
+};
+
+export const imTheme: IMTheme = {
+  name: 'default',
+  fontSize: {
+    lg: '1.125',
+    normal: '1',
+    sm: '0.7'
+  },
+  color: colors,
+  mediaQueries: {
+    whenMobile: '@media (max-width: 575px)',
+    whenMobileL: '@media (min-width: 576px)',
+    whenTablet: '@media (min-width: 768px)',
+    whenDesktop: '@media (min-width: 1024px)',
+    whenDesktopL: '@media (min-width: 1280px)',
+    whenDesktopXL: '@media (min-width: 1440px)',
+    whenDesktopXXL: '@media (min-width: 1680px)'
+  },
+  table: {
+    outerBorder: {
+      color: '#C4C4C4',
+      width: 2,
+      radius: 4
+    },
+    innerBorder: {
+      color: '#C4C4C4',
+      width: 1,
+      radius: 0
+    },
+    head: {
+      font: {
+        ...sansSerifFont,
+        size: 12,
+        height: 28,
+        spacing: 0.4,
+        weight: 500
+      },
+      background: '#FFFFFF'
+    },
+    body: {
+      font: {
+        ...sansSerifFont,
+        size: 12,
+        height: 28,
+        spacing: 0.4
+      },
+      background: '#FFFFFF'
+    },
+    foot: {
+      font: {
+        ...sansSerifFont,
+        size: 12,
+        height: 28,
+        spacing: 0.4,
+        weight: 500
+      },
+      background: '#FFFFFF'
+    }
+  },
+  card: {
+    border: {
+      radius: 4
+    },
+    shadow: '0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.1)'
+  },
+  button: {
+    primary: {
+      border: {
+        color: colors.primary,
+        width: 0
+      },
+      color: colors.white,
+      background: colors.primary,
+      padding: '0.8125rem 3rem',
+      font: {
+        ...sansSerifFont,
+        size: 1,
+        weight: 600,
+        spacing: 0
+      },
+      hover: {
+        shadow: '0 0.25rem 0.25rem rgba(0, 0 ,0, 0.1)'
+      },
+      active: {
+        shadow: 'inset 0px 0.125rem 0.125rem rgba(0, 0, 0, 0.05);'
+      },
+      disabled: {
+        opacity: 0.5
+      }
+    },
+    secondary: {
+      border: {
+        color: colors.secondary,
+        width: 0
+      },
+      color: colors.oil,
+      background: colors.secondary,
+      padding: '0.8125rem 3rem',
+      font: {
+        ...sansSerifFont,
+        size: 1,
+        weight: 600,
+        spacing: 0
+      },
+      hover: {
+        shadow: '0 0.25rem 0.25rem rgba(0, 0 ,0, 0.1)'
+      },
+      active: {
+        shadow: 'inset 0px 0.125rem 0.125rem rgba(0, 0, 0, 0.05);'
+      },
+      disabled: {
+        opacity: 0.5
+      }
+    },
+    grayscale: {
+      border: {
+        color: colors.white,
+        width: 0
+      },
+      color: colors.oil,
+      background: colors.white,
+      padding: '0.8125rem 3rem',
+      font: {
+        ...sansSerifFont,
+        size: 1,
+        weight: 600,
+        spacing: 0
+      },
+      hover: {
+        shadow: '0 0.25rem 0.25rem rgba(0, 0 ,0, 0.1)'
+      },
+      active: {
+        shadow: 'inset 0px 0.125rem 0.125rem rgba(0, 0, 0, 0.05);'
+      },
+      disabled: {
+        opacity: 0.5
+      }
+    },
+    outlined: {
+      border: {
+        color: colors.secondary,
+        width: 0.1225
+      },
+      color: colors.oil,
+      background: colors.white,
+      padding: '0.8125rem 3rem',
+      font: {
+        ...sansSerifFont,
+        size: 1,
+        weight: 600,
+        spacing: 0
+      },
+      hover: {
+        shadow: '0 0.25rem 0.25rem rgba(0, 0 ,0, 0.1)'
+      },
+      active: {
+        shadow: 'inset 0px 0.125rem 0.125rem rgba(0, 0, 0, 0.05);'
+      },
+      disabled: {
+        opacity: 0.5
+      }
+    }
+  },
+  input: {
+    border: {
+      width: 1,
+      color: '#C4C4C4'
+    },
+    font: {
+      ...sansSerifFont,
+      size: 16
+    }
+  },
+  label: {
+    font: {
+      ...sansSerifFont,
+      size: 1,
+      weight: 600
+    },
+    // line-height css rule
+    line: {
+      height: 1.15
+    },
+    // letter-spacing css rule
+    letter: {
+      spacing: 0.01
+    }
+  },
+  heading: {
+    h1Mobile: 32,
+    h1: 48,
+    h2Mobile: 24,
+    h2: 32,
+    h3Mobile: 18,
+    h3: 24,
+    h4Mobile: 18,
+    h4: 18
+  },
+  font: {
+    sans: sansSerifFont,
+    bentonRegular: BentonRegular,
+    bentonMedium: BentonMedium,
+    bentonBold: BentonBold,
+    bentonBlack: BentonBlack
+  },
+  footer: {
+    maxWidth: 1320
+  }
+};
+
+export default imTheme;
