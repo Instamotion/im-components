a7aa33432e957202f501edce925c80af07e65159
AntonTretiak <tretiak.anton.dev@gmail.com>
2021-02-19 12:34:43 +0200

    add footer storybook

--- packages/footer/stories/footer.stories.tsx
+++ packages/footer/stories/footer.stories.tsx
@@ -6,9 +6,16 @@ import Footer, { FooterVariant } from '../src';
 import messages from '../utils/locales';
 
 storiesOf('Complex | Footer', module)
-  .add('Full', () =>
+  .add('old', () =>
     renderWithThemeAndI18n(
-      <Footer showEnvkv variant={FooterVariant.full} googleToken="123" facebookToken="qwe" />,
+      <Footer showEnvkv variant={FooterVariant.old} googleToken="123" facebookToken="qwe" />,
+      'de',
+      messages
+    )
+  )
+  .add('new', () =>
+    renderWithThemeAndI18n(
+      <Footer showEnvkv variant={FooterVariant.new} googleToken="123" facebookToken="qwe" />,
       'de',
       messages
     )
