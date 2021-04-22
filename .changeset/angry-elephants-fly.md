c9b8993b70d985db21e101af82cace3f5971f7e8
Karlo Sintic <karlo.sintic@gmail.com>
2021-04-22 13:18:30 +0200

    add icon (#239)

---
+++ .changeset/angry-elephants-fly.md
@@ -0,0 +1,5 @@
+---
+'@im-ui/icon': patch
+---
+
+Add radioCheck icon
--- packages/icon/src/icon.tsx
+++ packages/icon/src/icon.tsx
@@ -1581,6 +1581,18 @@ export const Icons = {
         fill="currentColor"
       />
     </IconStyled>
+  ),
+  radioCheck: (props: IconProps) => (
+    <IconStyled viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M7.99998 1.33334C4.31998 1.33334 1.33331 4.32001 1.33331 8.00001C1.33331 11.68 4.31998 14.6667 7.99998 14.6667C11.68 14.6667 14.6666 11.68 14.6666 8.00001C14.6666 4.32001 11.68 1.33334 7.99998 1.33334Z"
+        fill="#76DFBF"
+      />
+      <path
+        d="M6.66667 9.72668L11.06 5.33334L12 6.28001L6.66667 11.6133L4 8.94668L4.94 8.00668L6.66667 9.72668Z"
+        fill="white"
+      />
+    </IconStyled>
   )
 };
 
