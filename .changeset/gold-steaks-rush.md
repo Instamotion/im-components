e5b37dbb758e5e53c8ea7f0c181b5682919080e6
Karlo Sintic <karlo.sintic@gmail.com>
2021-04-23 15:06:51 +0200

    [UT-855] Add currency input (#240)
    * Add currency input
    
    * Add snapshot

---
+++ .changeset/gold-steaks-rush.md
@@ -0,0 +1,5 @@
+---
+'@im-ui/input': patch
+---
+
+Add currency input
--- packages/input/src/index.ts
+++ packages/input/src/index.ts
@@ -2,3 +2,4 @@ export { InputProps } from './input';
 export { phoneRegex, checkPhoneValidation } from './phone';
 export { default } from './root';
 export { InputComponentWrapper } from './styles';
+export { CurrencyInput } from './currency';
