b132fb23d6b2148ef9d8a7233372225a55c32447
AntonTretiak <72020768+AntonTretiak@users.noreply.github.com>
2021-06-15 16:20:44 +0300

    update accordion (#276)

---
+++ .changeset/fifty-pandas-scream.md
@@ -0,0 +1,5 @@
+---
+'@im-ui/accordion': minor
+---
+
+add icons to accordion
--- packages/accordion/src/accordionItem.tsx
+++ packages/accordion/src/accordionItem.tsx
@@ -3,23 +3,35 @@ import styled, { css } from 'styled-components';
 import { IMTheme as theme } from '@im-ui/theme';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/pro-light-svg-icons';
+import Icon, { AvailableIcons } from '@im-ui/icon';
 
 export interface AccordionItemProps {
+  icon?: JSX.Element | AvailableIcons;
   title?: JSX.Element | string;
   open?: boolean;
 }
 
-const AccordionItem: React.FC<AccordionItemProps> = ({ title, open = false, children }) => {
+const AccordionItem: React.FC<AccordionItemProps> = ({ icon, title, open = false, children }) => {
   const [isOpen, setIsOpen] = React.useState(open);
 
   const handleClick = (): void => {
     setIsOpen(!isOpen);
   };
 
+  const getIcon = () => {
+    if (typeof icon === 'string') {
+      return <Icon icon={icon} color={theme.color.black} />;
+    }
+    return icon;
+  };
+
   return (
     <AccordionItemWrapper>
       <AccordionHeader aria-expanded={isOpen} onClick={handleClick}>
-        <AccordionTitle>{!!title && title}</AccordionTitle>
+        <IconSection>
+          {getIcon()}
+          <AccordionTitle>{!!title && title}</AccordionTitle>
+        </IconSection>
         <AccordionIcon icon={faChevronDown} open={isOpen} />
       </AccordionHeader>
       {isOpen && <AccordionItemContent>{children}</AccordionItemContent>}
@@ -29,44 +41,42 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, open = false, chil
 
 export const AccordionItemWrapper = styled.div`
   width: 100%;
-  padding: 0;
-  ${theme.mediaQueries.whenDesktop} {
-    padding: 0;
-  }
-  margin-bottom: 1rem;
   box-sizing: border-box;
-  border-bottom: 0.0625rem solid #e6e6e6;
+  border-top: 1px solid ${theme.color.whiteGrey};
+  font-family: ${theme.font.bentonMedium.family};
+  font-weight: ${theme.font.bentonMedium.weight};
+  font-size: 0.875rem;
+  color: ${theme.color.typography};
+
+  &:last-child {
+    border-bottom: 1px solid ${theme.color.whiteGrey};
+  }
 `;
 
 export const AccordionHeader = styled.div`
   display: flex;
   justify-content: space-between;
   cursor: pointer;
-  padding: 1rem 0 0;
-  :last-child {
-    padding-bottom: 1rem;
-  }
-  ${theme.mediaQueries.whenDesktop} {
-    padding: 1rem 1.5rem 0;
-    :last-child {
-      padding-bottom: 1rem;
-    }
+  padding: 1rem 2.25rem 1rem 0.625rem;
+`;
+
+export const IconSection = styled.div`
+  display: flex;
+  align-items: center;
+
+  > svg {
+    margin-right: 1rem;
   }
 `;
 
 export const AccordionTitle = styled.div`
-  color: ${theme.color.brightGrey};
   font-weight: bold;
   font-size: 1rem;
-  ${theme.mediaQueries.whenDesktop} {
-    font-size: 1.5rem;
-  }
 `;
 
 export const AccordionIcon = styled(FontAwesomeIcon)<{ open: boolean }>`
-  color: ${theme.color.brightGrey};
   user-select: none;
-  transform: rotate(-90deg);
+  transform: rotate(0);
   transition: transform 0.3s;
   font-size: 1rem;
   ${theme.mediaQueries.whenDesktop} {
@@ -75,16 +85,12 @@ export const AccordionIcon = styled(FontAwesomeIcon)<{ open: boolean }>`
   ${(props: { open: boolean }) =>
     props.open &&
     css`
-      transform: rotate(0);
+      transform: rotate(-180deg);
     `}
 `;
 
 export const AccordionItemContent = styled.div`
-  padding: 0;
-  ${theme.mediaQueries.whenDesktop} {
-    padding: 0 1.5rem;
-  }
-  color: ${theme.color.oil};
+  padding: 0 2.25rem 0 0.625rem;
 `;
 
 export default AccordionItem;
